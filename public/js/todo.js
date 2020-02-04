c09906d2598ba36466451edad87aeda072eab366
Aaquib Equbal <44823431+armanaaquib@users.noreply.github.com>
2020-02-04 07:39:34 +0530

    Added: Shwoing title after adding it

--- public/js/todo.js
+++ public/js/todo.js
@@ -1,3 +1,26 @@
+let todoList = [];
+
+const updateToHtml = function (todo) {
+  return `<h2 class="nav-item">${todo.title}</h2>`;
+};
+
+const renderNav = function () {
+  const navItemsAsHtml = todoList.map(updateToHtml).join('');
+  document.querySelector('#nav-items').innerHTML = navItemsAsHtml;
+};
+
+const updatePage = function () {
+  const req = new XMLHttpRequest();
+  req.onload = function () {
+    if (this.status == 200) {
+      todoList = JSON.parse(this.responseText);
+      renderNav();
+    }
+  }
+  req.open('GET', '/todoList');
+  req.send();
+};
+
 const addTitle = function () {
   const titleElement = document.querySelector('#title-text');
   const titleText = titleElement.value;
@@ -9,6 +32,11 @@ const addTitle = function () {
   titleElement.value = ''
 
   const req = new XMLHttpRequest();
+  req.onload = function () {
+    if (this.status == 200) {
+      updatePage();
+    }
+  }
   req.open('POST', '/addTitle');
   req.send(titleText);
 };
