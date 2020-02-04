fd4fa2af7b428a7ea205ece59947c625b08015be
Aaquib Equbal <44823431+armanaaquib@users.noreply.github.com>
2020-02-04 22:27:39 +0530

    Added: testList class
    added webRequest for get and post

--- public/js/todo.js
+++ public/js/todo.js
@@ -1,42 +1,23 @@
-let todoList = [];
-
-const updateToHtml = function (todo) {
-  return `<h2 class="nav-item">${todo.title}</h2>`;
+const renderTitleItems = function (todoList) {
+  document.querySelector('#nav-items').innerHTML = todoList.titlesHtml();
 };
 
-const renderNav = function () {
-  const navItemsAsHtml = todoList.map(updateToHtml).join('');
-  document.querySelector('#nav-items').innerHTML = navItemsAsHtml;
-};
+const updateTitleItems = function () {
+  sendGetRequest('todoList', (responseText) => {
+    const todoList = TodoList.load(responseText);
+    renderTitleItems(todoList);
+  });
+}
 
-const updatePage = function () {
-  const req = new XMLHttpRequest();
-  req.onload = function () {
-    if (this.status == 200) {
-      todoList = JSON.parse(this.responseText);
-      renderNav();
-    }
-  }
-  req.open('GET', '/todoList');
-  req.send();
-};
-
-const addTitle = function () {
+const addTodo = function () {
   const titleElement = document.querySelector('#title-text');
   const titleText = titleElement.value;
 
   if (titleText === '') {
-    alert('Please Enter Title')
+    alert('Please Enter Title');
+    return;
   }
-
   titleElement.value = ''
 
-  const req = new XMLHttpRequest();
-  req.onload = function () {
-    if (this.status == 200) {
-      updatePage();
-    }
-  }
-  req.open('POST', '/addTitle');
-  req.send(titleText);
+  sendPostRequest('addTodo', titleText, 'text/plain', updateTitleItems);
 };
---
+++ public/js/todoList.js
@@ -0,0 +1,40 @@
+const taskHtml = function (task) {
+  return `
+  <div class="task" id="${task.id}">
+    <input class="tick-mark" type="checkbox" ${task.status ? 'checked' : ''}>
+    <label>${task.text}</label>
+    <a class='delete-link' href="">Delete</a>
+  </div>
+  <br>`;
+};
+
+const titleHtml = function (todo) {
+  return `<h2 class="nav-item">${todo.title}</h2>`;
+};
+
+class TodoList {
+  constructor (todoList) {
+    this.todoList = todoList;
+  }
+
+  static load(content) {
+    const list = JSON.parse(content);
+    const todoList = new TodoList(list);
+    return todoList;
+  }
+
+  titlesHtml() {
+    const reverseTodoList = this.todoList.slice().reverse();
+    return reverseTodoList.map((todo) => titleHtml(todo)).join('');
+  }
+
+  tasksHtml(title) {
+    const todo = this.todoList.find((todo) => todo.title === title);
+
+    const newTaskHtml = `
+    <input id="task-text" type="text" placeholder=" Enter Task" required>
+    <input id="task-button" type="button" value="Add Task" />`;
+
+    return todo.tasks.map((task) => taskHtml(task)).join('') + newTaskHtml;
+  }
+}
