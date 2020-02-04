1301e13854471f041294db6f6f6ef686c7566517
Aaquib Equbal <44823431+armanaaquib@users.noreply.github.com>
2020-02-04 07:19:10 +0530

    Updated: addTitle(), Now clearing value of title after adding

--- public/js/todo.js
+++ public/js/todo.js
@@ -1,5 +1,13 @@
 const addTitle = function () {
-  const titleText = document.querySelector('#title-text').value;
+  const titleElement = document.querySelector('#title-text');
+  const titleText = titleElement.value;
+
+  if (titleText === '') {
+    alert('Please Enter Title')
+  }
+
+  titleElement.value = ''
+
   const req = new XMLHttpRequest();
   req.open('POST', '/addTitle');
   req.send(titleText);
