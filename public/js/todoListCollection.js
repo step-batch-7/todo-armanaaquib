479998526a1304bdc6f28a52658188ad03555a84
Aaquib Equbal <44823431+armanaaquib@users.noreply.github.com>
2020-02-10 10:09:29 +0530

    Fixed: bug while editing tiitle

--- public/js/todoListCollection.js
+++ public/js/todoListCollection.js
@@ -15,8 +15,8 @@ const taskHtml = function (task) {
 const titleHtml = function (todoList) {
   return `
   <div class="nav-item" id="${todoList.id}" onclick="clickedTodo(event);">
-    <h2 class="todo-title" contentEditable="true" onblur="editTitle(event);">${todoList.title}</h2>
-    <a class='delete-link' onclick="removeTodo(event)">X</a>
+    <h2 class="todo-title" id="${todoList.id}" contentEditable="true" onblur="editTitle(event);">${todoList.title}</h2>
+    <a class='delete-link' id="${todoList.id}" onclick="removeTodo(event)">X</a>
   </div>`;
 };
 
